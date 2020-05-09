c02e00d93989691e17939a967669e85d85770d17
Jason Junio <jjjunio@gmail.com>
2020-05-09 12:52:55 +0000

    created nextISSTimesForMyLocation function to finish the ISS Spotter challenge

--- index.js
+++ index.js
@@ -4,35 +4,35 @@ const { fetchCoordsByIP } = require('./iss');
 const { fetchISSFlyOverTimes } = require('./iss');
 const { nextISSTimesForMYLocation } = require('./iss');
 
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
-//   console.log('It worked! Returned IP:' , ip);
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+  console.log('It worked! Returned IP:' , ip);
   
-// });
+});
 
-// fetchCoordsByIP("65.94.166.27", (error, coords) => {
-//   if (error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+fetchCoordsByIP("65.94.166.27", (error, coords) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
 
-//   console.log('It worked! Your coordinates are:' , coords);
-// });
+  console.log('It worked! Your coordinates are:' , coords);
+});
 
-// const myCoords = { latitude: "45.46670" , longitude: "-73.88330" }
+const myCoords = { latitude: "45.46670" , longitude: "-73.88330" }
 
-// fetchISSFlyOverTimes(myCoords, (error, flyOverData) => {
+fetchISSFlyOverTimes(myCoords, (error, flyOverData) => {
 
-//   if(error) {
-//     console.log("It didn't work!" , error);
-//     return;
-//   }
+  if(error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
   
-//   console.log("It worked! Here are the next ISS pass times for your coordinates:", flyOverData);
-// });
+  console.log("It worked! Here are the next ISS pass times for your coordinates:", flyOverData);
+});
 
 const readableFlyOverTimes = function(flyOverData) {
   for (let times of flyOverData) {
